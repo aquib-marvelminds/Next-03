97551dae6a0d4d0f8cdbfbea45e8e29ac59c14a5
Mehran <mehranbhat2001@gmail.com>
2024-05-09 18:30:08 +0530

    wip

--- myapp/src/app/components/Footer.tsx
+++ myapp/src/app/components/Footer.tsx
@@ -3,7 +3,32 @@ import React from "react";
 const Footer = () => {
   return (
     <>
-      <div></div>
+      <div className="h-[20rem] flex justify-evenly">
+        <div>
+          <h1 className="text-2xl">Follow Me On</h1>
+          <ul className="text-2md ">
+            <li>Twitter</li>
+            <li>Facebook</li>
+            <li>Instagram</li>
+          </ul>
+        </div>
+        <div>
+          <h1 className="text-2xl">Quick links</h1>
+          <ul className="text-2md ">
+            <li>Home</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+        <div>
+          <h1 className="text-2xl">Contact Us</h1>
+          <ul className="text-2md ">
+            <li>London UK</li>
+            <li>J&K, 190002</li>
+            <li>Email: mehranbhat2001@gmail.com</li>
+            <li>Phone: (+91) 6005046534 </li>
+          </ul>
+        </div>
+      </div>
     </>
   );
 };
