eae6d42321f8607a0af64d6c696b3cef46a816cc
Mehran <mehranbhat2001@gmail.com>
2024-05-09 18:42:47 +0530

    wip

--- myapp/src/app/components/Footer.tsx
+++ myapp/src/app/components/Footer.tsx
@@ -3,32 +3,35 @@ import React from "react";
 const Footer = () => {
   return (
     <>
-      <div className="h-[20rem] flex justify-evenly">
-        <div>
-          <h1 className="text-2xl">Follow Me On</h1>
-          <ul className="text-2md ">
+      <div className="md:h-[20rem] flex flex-col md:flex-row justify-evenly items-center md:items-start md:text-left md:space-x-10">
+        <div className="mb-5 md:mb-0">
+          <h1 className="text-2xl mb-4">Follow Me On</h1>
+          <ul className="text-2md">
             <li>Twitter</li>
             <li>Facebook</li>
             <li>Instagram</li>
           </ul>
         </div>
-        <div>
-          <h1 className="text-2xl">Quick links</h1>
-          <ul className="text-2md ">
+        <div className="mb-5 md:mb-0">
+          <h1 className="text-2xl mb-4">Quick links</h1>
+          <ul className="text-2md">
             <li>Home</li>
             <li>Contact</li>
           </ul>
         </div>
         <div>
-          <h1 className="text-2xl">Contact Us</h1>
-          <ul className="text-2md ">
+          <h1 className="text-2xl mb-4">Contact Us</h1>
+          <ul className="text-2md">
             <li>London UK</li>
             <li>J&K, 190002</li>
             <li>Email: mehranbhat2001@gmail.com</li>
-            <li>Phone: (+91) 6005046534 </li>
+            <li>Phone: (+91) 6005046534</li>
           </ul>
         </div>
       </div>
+      <h4 className="mt-5 md:mt-10 text-center">
+        © Copyright. All rights reserved
+      </h4>
     </>
   );
 };
