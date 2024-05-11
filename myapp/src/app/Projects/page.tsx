f452ff3ada5aff997e210ec17e29ece0d6dd9009
Mehran <mehranbhat2001@gmail.com>
2024-05-11 13:14:13 +0530

    wip

--- myapp/src/app/Projects/page.tsx
+++ myapp/src/app/Projects/page.tsx
@@ -16,14 +16,14 @@ const page = () => {
               translateZ="50"
               className="text-xl font-bold text-neutral-600 dark:text-white"
             >
-              Make things float in air
+              Meeting APP
             </CardItem>
             <CardItem
               as="p"
               translateZ="60"
               className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
             >
-              Hover over this card to unleash the power of CSS perspective
+              Web Applications (D0ne web)
             </CardItem>
             <CardItem translateZ="100" className="w-full mt-4">
               <Image
@@ -35,7 +35,7 @@ const page = () => {
               />
             </CardItem>
             <div className="flex justify-between items-center mt-20">
-              <CardItem
+              {/* <CardItem
                 translateZ={20}
                 as={Link}
                 href="https://twitter.com/mannupaaji"
@@ -43,13 +43,13 @@ const page = () => {
                 className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
               >
                 Try now →
-              </CardItem>
+              </CardItem> */}
               <CardItem
                 translateZ={20}
                 as="button"
                 className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
               >
-                Sign up
+                Visit →
               </CardItem>
             </div>
           </CardBody>
