2092ede4fb33315bb16a44ca53c95e4260972b6a
Mehran <mehranbhat2001@gmail.com>
2024-05-12 13:08:29 +0530

    wip

--- myapp/src/app/components/Background.tsx
+++ myapp/src/app/components/Background.tsx
@@ -1,4 +1,5 @@
 "use client";
+import React from "react";
 import Link from "next/link";
 import courseData from "../data/music_courses.json";
 import { BackgroundGradient } from "./ui/background-gradient";
@@ -24,7 +25,7 @@ const Background = () => {
             ABOUT ME
           </h2>
           <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-white sm:text-4xl">
-            Let's Get Started
+            {`Let's Get Started`}
           </p>
         </div>
       </div>
@@ -40,7 +41,9 @@ const Background = () => {
                   <p className="text-sm text-neutral-600 dark:text-neutral-400 flex-grow">
                     {course.description}
                   </p>
-                  <Link href={`/courses/${course.slug}`}>Learn More</Link>
+                  <Link href={{ pathname: `/courses/${course.slug}` }}>
+                    Learn More
+                  </Link>
                 </div>
               </BackgroundGradient>
             </div>
@@ -59,79 +62,3 @@ const Background = () => {
   );
 };
 export default Background;
-{
-  /* <div className="flex-col flex">
-  <BackgroundGradient className="rounded-[22px] max-w-sm p-4 sm:p-10 bg-white dark:bg-zinc-900">
-    <Image
-      src={img}
-      alt="jordans"
-      height="400"
-      width="400"
-      className="object-contain"
-    />
-    <p className="text-base sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">
-      Air Jordan 4 Retro Reimagined
-    </p>
-
-    <p className="text-sm text-neutral-600 dark:text-neutral-400">
-      The Air Jordan 4 Retro Reimagined Bred will release on Saturday,
-      February 17, 2024. Your best opportunity to get these right now is
-      by entering raffles and waiting for the official releases.
-    </p>
-    <button className="rounded-full pl-4 pr-1 py-1 text-white flex items-center space-x-1 bg-black mt-4 text-xs font-bold dark:bg-zinc-800">
-      <span>Buy now </span>
-      <span className="bg-zinc-700 rounded-full text-[0.6rem] px-2 py-0 text-white">
-        $100
-      </span>
-    </button>
-  </BackgroundGradient>
-  <BackgroundGradient className="rounded-[22px] max-w-sm p-4 sm:p-10 bg-white dark:bg-zinc-900">
-    <Image
-      src={fire}
-      alt="jordans"
-      height="400"
-      width="400"
-      className="object-contain"
-    />
-    <p className="text-base sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">
-      Air Jordan 4 Retro Reimagined
-    </p>
-
-    <p className="text-sm text-neutral-600 dark:text-neutral-400">
-      The Air Jordan 4 Retro Reimagined Bred will release on Saturday,
-      February 17, 2024. Your best opportunity to get these right now is
-      by entering raffles and waiting for the official releases.
-    </p>
-    <button className="rounded-full pl-4 pr-1 py-1 text-white flex items-center space-x-1 bg-black mt-4 text-xs font-bold dark:bg-zinc-800">
-      <span>Buy now </span>
-      <span className="bg-zinc-700 rounded-full text-[0.6rem] px-2 py-0 text-white">
-        $100
-      </span>
-    </button>
-  </BackgroundGradient>
-  <BackgroundGradient className="rounded-[22px] max-w-sm p-4 sm:p-10 bg-white dark:bg-zinc-900">
-    <Image
-      src={zub}
-      alt="jordans"
-      height="400"
-      width="400"
-      className="object-contain"
-    />
-    <p className="text-base sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">
-      Air Jordan 4 Retro Reimagined
-    </p>
-
-    <p className="text-sm text-neutral-600 dark:text-neutral-400">
-      The Air Jordan 4 Retro Reimagined Bred will release on Saturday,
-      February 17, 2024. Your best opportunity to get these right now is
-      by entering raffles and waiting for the official releases.
-    </p>
-    <button className="rounded-full pl-4 pr-1 py-1 text-white flex items-center space-x-1 bg-black mt-4 text-xs font-bold dark:bg-zinc-800">
-      <span>Buy now </span>
-      <span className="bg-zinc-700 rounded-full text-[0.6rem] px-2 py-0 text-white">
-        $100
-      </span>
-    </button>
-  </BackgroundGradient>
-</div> */
-}
